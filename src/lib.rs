4c81d7c0dc39ec33b86cc9295c7ac3631dc8ad95
Steven Casper <sebastiancasper3@gmail.com>
2023-02-01 20:47:01 -0800

    Rehook if attaching fails

--- src/lib.rs
+++ src/lib.rs
@@ -10,33 +10,46 @@ const GAME_INSTANCE_OFFSET: u64 = 0xD28;
 const LOAD_PATH: [u64; 5] = [GAME_ENGINE_OFFSET, GAME_INSTANCE_OFFSET, 0xF0, 0xE0, 0xA0];
 
 // TODO: Mutex shouldn't be necessary as this is all single-threaded (double-check this)
-static STATE: Lazy<Mutex<State>> = Lazy::new(|| {
-    Mutex::new(State {
-        // TODO: do this in update, deal with fallability
-        process: Process::attach("CosmicShake-Win64-Shipping.exe")
-            .expect("Could not attach to process"),
-        is_loading: Watcher::new(),
-    })
-});
+static STATE: Lazy<Mutex<State>> = Lazy::new(|| Mutex::new(State { game: None }));
 
 struct State {
+    game: Option<Game>,
+}
+
+struct Game {
     process: Process,
     is_loading: Watcher<u8>,
+}
+
+impl Game {
+    fn attach() -> Option<Self> {
+        Some(Self {
+            process: Process::attach("CosmicShake-Win64-Shipping.exe")?,
+            is_loading: Watcher::new(),
+        })
+    }
 }
 
 #[no_mangle]
 pub extern "C" fn update() {
     let mut state = STATE.lock().unwrap();
-    let is_loading = dbg!(state.process.read_pointer_path64(
-        state
-            .process
-            .get_module_address("CosmicShake-Win64-Shipping.exe")
-            .unwrap()
-            .0,
-        &LOAD_PATH,
-    ))
-    .ok();
-    state.is_loading.update(is_loading);
+    // TODO: Rehooking after game restart
+    let Some(game) = &mut state.game else {
+        state.game = Game::attach();
+        return;
+    };
+
+    let is_loading = game
+        .process
+        .read_pointer_path64(
+            game.process
+                .get_module_address("CosmicShake-Win64-Shipping.exe")
+                .unwrap()
+                .0,
+            &LOAD_PATH,
+        )
+        .ok();
+    game.is_loading.update(is_loading);
 
     match is_loading {
         Some(x) if x != 0 => asr::timer::pause_game_time(),
